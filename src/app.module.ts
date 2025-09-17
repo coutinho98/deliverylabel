e1032c756542436a1b7a1862e619143a740a30a9
coutinho98 <mateuscouto98@outlook.com>
2025-09-16 21:02:20 -0300

    create a user resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RestaurantModule } from './restaurant/restaurant.module';
 import { PrismaModule } from './prisma/prisma.module'; 
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [RestaurantModule, PrismaModule],
+  imports: [RestaurantModule, PrismaModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
